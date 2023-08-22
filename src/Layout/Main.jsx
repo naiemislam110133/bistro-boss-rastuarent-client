11b10f4568bdee5e9eb323ece15d40cf5cd08663
Naiem Islam <naiemislam1547@gmail.com>
2023-08-23 04:21:20 +0600

    Added footer Ssection and ceate dynamice year and copy right sign

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+
+const Main = () => {
+    return (
+        <>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Main;
